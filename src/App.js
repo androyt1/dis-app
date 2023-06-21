04585763c82eec85ecb606323e33e552b32d0ba6
Andrew Aghoghovwia <androyt1@gmail.com>
2023-06-21 14:07:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,29 +3,38 @@ import React, { useState, useEffect } from 'react';
 function InfiniteScrollComponent() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
   // Your API call function
   const fetchData = async () => {
     setLoading(true);
-    // Make your API call here and append the response data to existing data
-    // Example: const response = await fetch('your-api-endpoint');
+    // Make your API call here with the page number
+    // Example: const response = await fetch(`your-api-endpoint?page=${page}`);
     //          const newData = await response.json();
     //          setData(prevData => [...prevData, ...newData]);
+    setPage(prevPage => prevPage + 1);
     setLoading(false);
   };
 
-  // useEffect hook to fetch data when the component mounts
+  // useEffect hook to fetch data when the component mounts and when the page changes
   useEffect(() => {
     fetchData();
-  }, []);
-  
+  }, [page]);
+
   // Function to handle scroll event
-  const handleScroll = () => {
-    // Calculate the scroll position
+  const handleScroll = event => {
     const { scrollTop, clientHeight, scrollHeight } = event.target;
+
+    // Calculate if the user has reached the top or bottom of the div
+    const isAtTop = scrollTop === 0;
     const isAtBottom = scrollTop + clientHeight === scrollHeight;
 
-    // Fetch more data if the user has reached the bottom and data is not already loading
+    // Fetch previous page if the user has reached the top and the current page is not the first page
+    if (isAtTop && page > 1 && !loading) {
+      setPage(prevPage => prevPage - 1);
+    }
+
+    // Fetch next page if the user has reached the bottom and data is not already loading
     if (isAtBottom && !loading) {
       fetchData();
     }
@@ -48,4 +57,3 @@ function InfiniteScrollComponent() {
 }
 
 export default InfiniteScrollComponent;
-                
