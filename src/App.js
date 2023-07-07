85b51939226c2a34fa544cab2519a1e9cd900fdd
Andrew Aghoghovwia <androyt1@gmail.com>
2023-07-08 00:19:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 function getChildProperties(json, prefix = '') {
   const properties = [];
 
-  for (const key in json) {
-    if (json.hasOwnProperty(key)) {
-      const value = json[key];
+  function traverseObject(obj, prefix = '') {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const value = obj[key];
 
-      if (key === 'properties') {
-        for (const childKey in value) {
-          if (value.hasOwnProperty(childKey)) {
-            const childValue = value[childKey];
-            if (childValue.hasOwnProperty('properties')) {
-              const nestedPrefix = prefix ? `${prefix}.${childKey}` : childKey;
-              const nestedProperties = getChildProperties(childValue.properties, nestedPrefix);
-              properties.push(...nestedProperties);
-            } else {
-              properties.push(`${prefix}${childKey}`);
+        if (key === 'properties') {
+          for (const childKey in value) {
+            if (value.hasOwnProperty(childKey)) {
+              const childValue = value[childKey];
+              if (childValue.hasOwnProperty('properties')) {
+                const nestedPrefix = prefix ? `${prefix}.${childKey}` : childKey;
+                traverseObject(childValue.properties, nestedPrefix);
+              } else {
+                properties.push(`${prefix}${childKey}`);
+              }
             }
           }
         }
       }
     }
   }
+
+  traverseObject(json, prefix);
 
   return properties;
 }
