02230fec4c1c693815f96bdafd8d3eb29b71c8a1
Andrew Aghoghovwia <androyt1@gmail.com>
2023-07-07 15:48:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-function displayTextOnAvailableSpace(divId, text) {
-  const div = document.getElementById(divId);
-  const divWidth = div.clientWidth;
-  let content = text;
+import React, { useEffect, useRef } from 'react';
+import './ResizableDiv.css';
 
-  while (div.scrollWidth > divWidth && content.length > 0) {
-    content = content.substring(0, content.length - 1);
-  }
+function ResizableDiv() {
+  const resizableDivRef = useRef(null);
 
-  div.innerText = content;
+  useEffect(() => {
+    const resizableDiv = resizableDivRef.current;
+    const textContent = resizableDiv.textContent;
+    
+    const adjustTextOnResize = () => {
+      resizableDiv.style.width = ''; // Reset width to auto
+      resizableDiv.style.width = resizableDiv.offsetWidth + 'px'; // Apply the current width in pixels
+
+      if (resizableDiv.scrollWidth > resizableDiv.clientWidth) {
+        let start = 0;
+        let end = textContent.length;
+
+        while (start <= end) {
+          const middle = Math.floor((start + end) / 2);
+          const truncatedText = textContent.substring(0, middle) + '...';
+
+          resizableDiv.textContent = truncatedText;
+
+          if (resizableDiv.scrollWidth > resizableDiv.clientWidth) {
+            end = middle - 1;
+          } else {
+            start = middle + 1;
+          }
+        }
+
+        const finalTruncatedText = textContent.substring(0, end) + '...';
+        resizableDiv.textContent = finalTruncatedText;
+      }
+    };
+
+    adjustTextOnResize();
+
+    window.addEventListener('resize', adjustTextOnResize);
+
+    return () => {
+      window.removeEventListener('resize', adjustTextOnResize);
+    };
+  }, []);
+
+  return (
+    <div ref={resizableDivRef} className="resizable-div">
+      This is some long text that needs to fit the available space with an ellipsis at the end if necessary.
+    </div>
+  );
 }
+
+export default ResizableDiv;
