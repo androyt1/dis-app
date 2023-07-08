0793412876addca7cb668bc85fbe94dfbbfca9fd
Andrew Aghoghovwia <androyt1@gmail.com>
2023-07-08 17:12:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-function getChildProperties(json, prefix = '') {
-  const properties = [];
-
-  function traverseObject(obj, prefix = '') {
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        const value = obj[key];
-
-        if (key === 'properties') {
-          for (const childKey in value) {
-            if (value.hasOwnProperty(childKey)) {
-              const childValue = value[childKey];
-              if (childValue.hasOwnProperty('properties')) {
-                const nestedPrefix = prefix ? `${prefix}.${childKey}` : childKey;
-                traverseObject(childValue.properties, nestedPrefix);
-              } else {
-                properties.push(`${prefix}${childKey}`);
-              }
+function getNestedPropertiesNames(obj) {
+    let propertyNames = [];
+  
+    function traverse(obj, path = '') {
+      if (typeof obj !== 'object' || obj === null) {
+        return;
+      }
+  
+      if ('properties' in obj) {
+        for (let key in obj.properties) {
+          if (typeof obj.properties[key] === 'object' && obj.properties[key] !== null) {
+            let newPath = path ? `${path}.${key}` : key;
+            if (key !== 'nested') {
+              propertyNames.push(newPath);
             }
+            traverse(obj.properties[key], newPath);
           }
-        } else if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
-          const nestedPrefix = prefix ? `${prefix}.${key}` : key;
-          traverseObject(value, nestedPrefix);
+        }
+      } else {
+        for (let key in obj) {
+          let newPath = path ? `${path}.${key}` : key;
+          traverse(obj[key], newPath);
         }
       }
     }
+  
+    traverse(obj);
+    return propertyNames;
   }
-
-  traverseObject(json, prefix);
-
-  return properties;
-}
