1631a3969fecb46632cf7a1d803f73fba5d96e06
Andrew Aghoghovwia <androyt1@gmail.com>
2023-07-26 07:18:52 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-const Accordion = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasMore, setHasMore] = useState(true);
-  const [pageNumber, setPageNumber] = useState(1);
+const ParentComponent = () => {
+  const [checkedCheckboxes, setCheckedCheckboxes] = useState({});
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // Function to handle checkboxes
+  const handleCheckboxes = (groupName, uniqueValue) => {
+    // Create a copy of the current checkedCheckboxes state
+    const newCheckedCheckboxes = { ...checkedCheckboxes };
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`https://api.example.com/data?page=${pageNumber}`);
-      const responseData = await response.json();
-
-      setData((prevData) => [...prevData, ...responseData]);
-      setIsLoading(false);
-      setHasMore(responseData.length > 0);
-    } catch (error) {
-      console.log('Error fetching data:', error);
+    // Check if the groupName already exists in the state
+    if (!newCheckedCheckboxes[groupName]) {
+      // If the groupName doesn't exist, create an empty array for it
+      newCheckedCheckboxes[groupName] = [];
     }
+
+    // Check if the uniqueValue exists in the groupName array
+    const index = newCheckedCheckboxes[groupName].indexOf(uniqueValue);
+
+    if (index !== -1) {
+      // If the uniqueValue exists in the array, remove it
+      newCheckedCheckboxes[groupName].splice(index, 1);
+    } else {
+      // If the uniqueValue doesn't exist in the array, add it
+      newCheckedCheckboxes[groupName].push(uniqueValue);
+    }
+
+    // Update the state with the new checked checkboxes
+    setCheckedCheckboxes(newCheckedCheckboxes);
   };
 
-  const handleScroll = () => {
-    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-      if (isLoading || !hasMore) return;
-
-      setPageNumber((prevPageNumber) => prevPageNumber + 1);
-      setIsLoading(true);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [isLoading, hasMore]);
-
-  return (
-    <div>
-      {data.map((item, index) => (
-        <div key={index}>
-          <input type="checkbox" value={item.name} /> {item.name}
-        </div>
-      ))}
-      {isLoading && <div>Loading...</div>}
-      {!isLoading && !hasMore && <div>No more items to load.</div>}
-    </div>
-  );
+  // Render your child components with checkboxes and pass the handleCheckboxes function as a prop
+  // Example:
+  // <ChildComponent1 handleCheckboxes={handleCheckboxes} />
+  // <ChildComponent2 handleCheckboxes={handleCheckboxes} />
+  // <ChildComponent3 handleCheckboxes={handleCheckboxes} />
+  // <ChildComponent4 handleCheckboxes={handleCheckboxes} />
 };
 
-export default Accordion;
+export default ParentComponent;
+
     
