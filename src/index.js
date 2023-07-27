59025b0a027e421304aeb109978e1fed3fcf637f
Andrew Aghoghovwia <androyt1@gmail.com>
2023-07-27 05:50:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const ParentComponent = () => {
-  const [checkedCheckboxes, setCheckedCheckboxes] = useState({});
-
-  // Function to handle checkboxes
-  const handleCheckboxes = (groupName, uniqueValue) => {
-    // Create a copy of the current checkedCheckboxes state
-    const newCheckedCheckboxes = { ...checkedCheckboxes };
-
-    // Check if the groupName already exists in the state
-    if (!newCheckedCheckboxes[groupName]) {
-      // If the groupName doesn't exist, create an empty array for it
-      newCheckedCheckboxes[groupName] = [];
-    }
-
-    // Check if the uniqueValue exists in the groupName array
-    const index = newCheckedCheckboxes[groupName].indexOf(uniqueValue);
-
-    if (index !== -1) {
-      // If the uniqueValue exists in the array, remove it
-      newCheckedCheckboxes[groupName].splice(index, 1);
-    } else {
-      // If the uniqueValue doesn't exist in the array, add it
-      newCheckedCheckboxes[groupName].push(uniqueValue);
-    }
-
-    // Update the state with the new checked checkboxes
-    setCheckedCheckboxes(newCheckedCheckboxes);
-  };
-
-  // Render your child components with checkboxes and pass the handleCheckboxes function as a prop
-  // Example:
-  // <ChildComponent1 handleCheckboxes={handleCheckboxes} />
-  // <ChildComponent2 handleCheckboxes={handleCheckboxes} />
-  // <ChildComponent3 handleCheckboxes={handleCheckboxes} />
-  // <ChildComponent4 handleCheckboxes={handleCheckboxes} />
+const CustomComponent = ({ componentId, checkboxesData, checkedItems, onCheckboxChange }) => {
+  return (
+    <div>
+      {checkboxesData.map((item) => (
+        <div key={item.id}>
+          <input
+            type="checkbox"
+            id={item.id}
+            checked={checkedItems[item.id] || false}
+            onChange={(e) => onCheckboxChange(componentId, item.id, e.target.checked)}
+          />
+          <label htmlFor={item.id}>{item.label}</label>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default ParentComponent;
-
-    
+export default CustomComponent;
