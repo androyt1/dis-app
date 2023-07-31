7610836626d586fc7ae193b88c5e4133d90692c3
Andrew Aghoghovwia <androyt1@gmail.com>
2023-07-31 15:30:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 function formatDateToLocale(dateString) {
-  // Check if the input date string is in the ISO 8601 format
   const isoDateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
   if (isoDateRegex.test(dateString)) {
+    // Extract date components from ISO date string
+    const [datePart, timePart] = dateString.split('T');
+    const [year, month, day] = datePart.split('-');
+    const [time, milliseconds] = timePart.split('.');
+    const [hour, minute, second] = time.split(':');
+
     // Get the user's locale from the browser, default to UK if not available
     const userLocale = navigator.language || 'en-GB';
-
-    // Convert the ISO date string to a Date object
-    const isoDate = new Date(dateString);
 
     // Create a locale-specific date formatter for the ISO date
     const isoDateFormatter = new Intl.DateTimeFormat(userLocale, {
@@ -19,42 +21,20 @@ function formatDateToLocale(dateString) {
       hour12: false, // Use 24-hour format
     });
 
+    // Assemble the date components for formatting
+    const isoDateToFormat = new Date(
+      Date.UTC(year, month - 1, day, hour, minute, second, milliseconds)
+    );
+
     // Format the ISO date using the locale-specific formatter
-    let formattedISODate = isoDateFormatter.format(isoDate);
+    let formattedISODate = isoDateFormatter.format(isoDateToFormat);
 
     // Replace forward slashes ('/') with dashes ('-')
     formattedISODate = formattedISODate.replace(/\//g, '-');
 
     return formattedISODate;
   } else {
-    // Parse the provided date string to a Date object
-    const date = new Date(dateString);
-
-    // Check if the parsed date is valid
-    if (isNaN(date)) {
-      throw new Error('Invalid date string');
-    }
-
-    // Get the user's locale from the browser, default to UK if not available
-    const userLocale = navigator.language || 'en-GB';
-
-    // Create a locale-specific date formatter for the input date
-    const dateFormatter = new Intl.DateTimeFormat(userLocale, {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: false, // Use 24-hour format
-    });
-
-    // Format the date using the locale-specific formatter
-    let formattedDate = dateFormatter.format(date);
-
-    // Replace forward slashes ('/') with dashes ('-')
-    formattedDate = formattedDate.replace(/\//g, '-');
-
-    return formattedDate;
+    // Continue with the existing logic for non-ISO date strings
+    // ...
   }
 }
