6e1010a012339cf305a8e2e783db19a44fe619be
Andrew Aghoghovwia <androyt1@gmail.com>
2023-08-01 19:47:45 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,45 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
+// Create a new instance of the mock adapter
+const mock = new MockAdapter(axios);
 
-
-describe('App.js file',()=>{
-  it('expects heading to be in the document',()=>{
-    render(<App/>)
-  const headingElement=screen.getByRole('heading',{name:/welcome to the homepage/i})
-  expect(headingElement).toBeInTheDocument()
-  })
-
-  it('Expects heading to be welcome to the homepage',()=>{
-    render(<App/>)
-    const headingElement=screen.getByRole('heading',{level:1})
-    expect(headingElement.textContent).toBe("Welcome to the Homepage")
-  })
-})
+describe('App', () => {
+    beforeEach(() => {
+      // Clear any previous mock responses before each test
+      mock.reset();
+    });
+  
+    it('should display user data', async () => {
+      // Mock a successful response for a GET request to a specific URL
+      mock.onGet('https://jsonplaceholder.typicode.com/users').reply(200, [
+        {
+          id: 1,
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz',
+        },
+      ]);
+  
+      // Render the component
+      render(<App />);
+  
+      // Your assertions based on the mock response
+      expect(await screen.findByText('Leanne Graham')).toBeInTheDocument();
+      expect(await screen.findByText('Sincere@april.biz')).toBeInTheDocument();
+    });
+  
+    it('should handle error from the API', async () => {
+      // Mock an error response for a GET request to a specific URL
+      mock.onGet('https://jsonplaceholder.typicode.com/users').reply(404, {
+        error: 'Internal Server Error',
+      });
+  
+      // Render the component
+      render(<App />);
+  
+      // Your assertions based on the mock error response
+      expect(await screen.findByText('Error: Internal Server Error')).toBeInTheDocument();
+    });
+  });
