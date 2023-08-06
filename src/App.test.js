b94622b269a04f500d46d9957899277e8d5e5b15
Andrew Aghoghovwia <androyt1@gmail.com>
2023-08-06 08:01:08 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,45 +1,61 @@
+// Import necessary dependencies and components
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-// Create a new instance of the mock adapter
+import YourComponent from './YourComponent';
+
 const mock = new MockAdapter(axios);
 
-describe('App', () => {
-    beforeEach(() => {
-      // Clear any previous mock responses before each test
-      mock.reset();
-    });
-  
-    it('should display user data', async () => {
-      // Mock a successful response for a GET request to a specific URL
-      mock.onGet('https://jsonplaceholder.typicode.com/users').reply(200, [
-        {
-          id: 1,
-          name: 'Leanne Graham',
-          username: 'Bret',
-          email: 'Sincere@april.biz',
-        },
-      ]);
-  
-      // Render the component
-      render(<App />);
-  
-      // Your assertions based on the mock response
-      expect(await screen.findByText('Leanne Graham')).toBeInTheDocument();
-      expect(await screen.findByText('Sincere@april.biz')).toBeInTheDocument();
-    });
-  
-    it('should handle error from the API', async () => {
-      // Mock an error response for a GET request to a specific URL
-      mock.onGet('https://jsonplaceholder.typicode.com/users').reply(404, {
-        error: 'Internal Server Error',
-      });
-  
-      // Render the component
-      render(<App />);
-  
-      // Your assertions based on the mock error response
-      expect(await screen.findByText('Error: Internal Server Error')).toBeInTheDocument();
-    });
+// Mock the successful API response
+mock.onGet('/api/data').reply(200, {
+  data: [/* your sample data here */],
+});
+
+// Mock the API response failure
+mock.onGet('/api/data').reply(500);
+
+// Mock the server-side search API
+mock.onGet('/api/search').reply(200, {
+  results: [/* your sample search results here */],
+});
+
+// Test scenarios
+describe('YourComponent', () => {
+  test('displays data from API response', async () => {
+    // Render the component
+    render(<YourComponent />);
+
+    // Wait for the API call to resolve (you may need to add a loading state check)
+    await waitFor(() => expect(screen.getByText('Data from API')).toBeInTheDocument());
+
+    // Assert that the data is displayed correctly in the component
+    expect(screen.getByText('Sample Data 1')).toBeInTheDocument();
+    expect(screen.getByText('Sample Data 2')).toBeInTheDocument();
+    // Add more assertions as needed
   });
+
+  test('displays error message on API failure', async () => {
+    // Render the component
+    render(<YourComponent />);
+
+    // Wait for the API call to fail (you may need to add a loading state check)
+    await waitFor(() => expect(screen.getByText('Error retrieving data.')).toBeInTheDocument());
+  });
+
+  test('displays search results correctly', async () => {
+    // Render the component
+    render(<YourComponent />);
+
+    // Get the search input textbox and type a query
+    const searchInput = screen.getByPlaceholderText('Search...');
+    fireEvent.change(searchInput, { target: { value: 'sample query' } });
+
+    // Wait for the server-side search to resolve (you may need to add a loading state check)
+    await waitFor(() => expect(screen.getByText('Search Results')).toBeInTheDocument());
+
+    // Assert that the search results are displayed correctly in the component
+    expect(screen.getByText('Sample Result 1')).toBeInTheDocument();
+    expect(screen.getByText('Sample Result 2')).toBeInTheDocument();
+    // Add more assertions as needed
+  });
+});
