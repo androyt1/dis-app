093dbe4f46bc4293ff698557eb008afe99c0ee97
Andrew Aghoghovwia <androyt1@gmail.com>
2023-08-10 08:00:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,32 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+// helpers.test.js (The test file)
 
+import { updateSteps } from './helpers';
 
-describe('createUserStates', () => {
-  it('returns initial values for all state variables', () => {
-    const { result } = renderHook(() => createUserStates());
-
-    expect(result.current.username.value).toBe('');
-    expect(result.current.email.value).toBe('');
-    expect(result.current.firstName.value).toBe('');
-    expect(result.current.lastName.value).toBe('');
-    expect(result.current.status.value).toBe('');
-    expect(result.current.roles.value).toBe('');
-    expect(result.current.users).toEqual([]);
+describe('updateSteps function', () => {
+  let mockSetSteps;
+  
+  beforeEach(() => {
+    mockSetSteps = jest.fn();
   });
 
-  it('sets a new value for username', () => {
-    const { result } = renderHook(() => createUserStates());
-    const newUsername = 'john.doe';
-
-    act(() => {
-      result.current.setUserName({ value: newUsername });
-    });
-
-    expect(result.current.username.value).toBe(newUsername);
+  test('should update steps when status is "SUCESSFUL"', () => {
+    updateSteps(mockSetSteps, 'SUCESSFUL');
+    
+    expect(mockSetSteps).toHaveBeenCalledTimes(1);
+    expect(mockSetSteps).toHaveBeenCalledWith(prevState => ({
+      ...prevState,
+      stepOne: false,
+      stepTwo: true,
+    }));
   });
 
-  it('updates users array when setUsers is called', () => {
-    const { result } = renderHook(() => createUserStates());
-    const newUsers = [{ id: 1, name: 'John' }];
-
-    act(() => {
-      result.current.setUsers(newUsers);
-    });
-
-    expect(result.current.users).toEqual(newUsers);
-  });
-
-  it('returns status options', () => {
-    const { result } = renderHook(() => createUserStates());
-
-    expect(result.current.status.statusOptions).toEqual([
-      { label: 'Active', value: 'Active' },
-      { label: 'Inactive', value: 'Inactive' },
-    ]);
-  });
-
-  it('returns role options', () => {
-    const { result } = renderHook(() => createUserStates());
-
-    expect(result.current.roles.roleOptions).toEqual([
-      // List of role options
-      // ...
-    ]);
+  test('should update steps when status is not "SUCESSFUL"', () => {
+    updateSteps(mockSetSteps, 'FAILURE');
+    
+    expect(mockSetSteps).toHaveBeenCalledTimes(1);
+    expect(mockSetSteps).toHaveBeenCalledWith(prevState => ({
+      ...prevState,
+      stepOne: false,
+    }));
   });
 });
